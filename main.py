d7a7fd0a11d146acef780e18622d366e0c6efec4
mayurvetoai <mayurdaulkar291@gmail.com>
2025-11-30 16:23:50 +0530

    addition+subtraction(in subtraction)

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import pandas as pd 
 import numpy as np 
+
+
+
 val1 = int(input("enter first value"))
 val2 = int(input("enter second value"))
-print(val1+val2)
+
+print("choose the thing you want to do")
+print("enter 1 for addtion::::")
+print("enter 2 for subtraction::::")
+user = int(input("select the number"))
+if  user ==1:
+    print(val1+val2)
+elif user ==2:
+    print(val1-val2)
+    
+    
+
+
+
+
+
